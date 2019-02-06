c097656b9f5b50d06ec64f604d20fe9e65404fab
Dan Groze <dangroze@gmail.com>
2019-02-06 14:16:27 +0000

    Added feature test for landing plane

---
+++ spec/featureSpec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe('Feature test:', function(){
+  var plane;
+  var airport;
+  beforeEach(function(){
+    plane = new Plane();
+    airport = new Airport();
+  });
+  it('Planes can be instructed to land at an airport', function(){
+    plane.land(airport);
+    expect(airport.planes()).toContain(plane);
+  });
+});
