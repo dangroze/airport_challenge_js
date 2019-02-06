e4a7e46c7d8f5f5e27dab80b69006af41c75d4e9
Dan Groze <dangroze@gmail.com>
2019-02-06 15:48:56 +0000

    Added unit test for plane takeoff

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -7,8 +7,13 @@ describe('Plane', function() {
     plane = new Plane();
     airport = jasmine.createSpyObj('airport',['clearForLanding']);
   });
-  it('can land at an airport', function(){
-    plane.land(airport)
+  it('Can land at an airport', function(){
+    plane.land(airport);
     expect(airport.clearForLanding).toHaveBeenCalledWith(plane);
+  });
+  it('Can take off from airport', function(){
+    plane.land(airport);
+    plane.takeoff();
+    expect(airport.clearForTakeOff()).toHaveBeenCalled();
   });
 });
