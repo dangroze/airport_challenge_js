0ed16c68fe2b826d6df4817d20dc38aab8a38ee0
Dan Groze <dangroze@gmail.com>
2019-02-07 11:54:30 +0000

    Added feature test for not taking off when stormy weather

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -3,17 +3,25 @@
 describe('Feature test:', function(){
   var plane;
   var airport;
+
   beforeEach(function(){
     plane = new Plane();
     airport = new Airport();
   });
+
   it('Planes can be instructed to land at an airport', function(){
     plane.land(airport);
     expect(airport.planes()).toContain(plane);
   });
   it('Planes can be instructed to take off', function(){
     plane.land(airport);
-    plane.takeoff(airport);
+    plane.takeoff();
     expect(airport.planes()).not.toContain(plane);
+  });
+  it('Planes cannot take off when stormy weather', function(){
+    plane.land(airport);
+    spyOn(airport,'isStormy').and.returnValue(true);
+    expect(function(){ plane.takeoff();}).toThrowError('Takeoff error: Bad Weather');
+    expect(airport.planes()).toContain(plane);
   });
 });
