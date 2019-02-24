b48795d9acb49a7e252ce6bd65c0ef564572b30a
Dan Groze <dangroze@gmail.com>
2019-02-24 19:09:34 +0000

    Test - weather can change

---
+++ spec/WeatherSpec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('Weather', function() {
+  var weather;
+  beforeEach(function() {
+    weather = new Weather();
+  })
+  it('can be stormy', function() {
+    spyOn(Math, 'random').and.returnValue(0);
+    expect(weather.isStormy()).toBeTruthy();
+  })
+  it('can be sunny', function() {
+    spyOn(Math, 'random').and.returnValue(0.5);
+    expect(weather.isStormy()).toBeFalsy();
+  })
+})
