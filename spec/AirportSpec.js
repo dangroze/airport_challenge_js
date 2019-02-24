8e1ea4964bd36da31ea95540b892acadac3ce856
Dan Groze <dangroze@gmail.com>
2019-02-24 19:13:23 +0000

    Refactored Airport spec

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -3,33 +3,38 @@
 describe('Airport', function() {
   var airport;
   var plane;
+  var weather;
   beforeEach(function() {
-    airport = new Airport();
-    plane = jasmine.createSpyObj('plane',['land']);
-  });
+    plane = jasmine.createSpy('plane');
+    weather = jasmine.createSpyObj('weather', ['isStormy']);
+    airport = new Airport(weather);
+  })
   it('has no planes by default', function() {
     expect(airport.planes()).toEqual([]);
-  });
-  it('can clear planes for landing', function() {
-    airport.clearForLanding(plane);
-    expect(airport.planes()).toEqual([plane]);
-  });
-  it('can clear planes for takeoff', function() {
-    airport.clearForLanding(plane);
-    airport.clearForTakeoff(plane);
-    expect(airport.planes()).toEqual([]);
-  });
-  it('can check for stormy weather', function() {
-    expect(airport.isStormy()).toBeFalsy();
-  });
-  describe('under stormy weather', function() {
-    it('does not clear for takeoff', function() {
-      spyOn(airport, 'isStormy').and.returnValue(true);
-      expect(function() {airport.clearForTakeoff(plane); }).toThrowError('Takeoff error: Bad Weather');
-    });
-    it('does not clear for landing', function() {
-      spyOn(airport, 'isStormy').and.returnValue(true);
-      expect(function() {airport.clearForLanding(plane); }).toThrowError('Landing error: Bad Weather');
+  })
+  describe('When sunny', function() {
+    beforeEach(function() {
+      weather.isStormy.and.returnValue(false);
     })
-  });
-});
+    it('can clear planes for landing', function() {
+      airport.clearForLanding(plane);
+      expect(airport.planes()).toEqual([plane]);
+    })
+    it('can clear planes for takeoff', function() {
+      airport.clearForLanding(plane);
+      airport.clearForTakeoff(plane);
+      expect(airport.planes()).toEqual([]);
+    })
+  })
+  describe('under stormy weather', function() {
+    beforeEach(function() {
+      weather.isStormy.and.returnValue(true);
+    })
+    it('does not clear for landing', function() {
+      expect(function() {airport.clearForLanding(plane); }).toThrowError('Landing error: Bad Weather')
+    })
+    it('does not clear for takeoff', function() {
+      expect(function() {airport.clearForTakeoff(plane); }).toThrowError('Takeoff error: Bad Weather')
+    })
+  })
+})
